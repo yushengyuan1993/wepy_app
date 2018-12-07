b3fa90c57b300778b11f896fe759b98ec1ef6121
yushare <582718643@qq.com>
2018-12-07 16:40:50 +0800

    init app

---
+++ wepy.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const LessPluginAutoPrefix = require('less-plugin-autoprefix')
+var prod = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  cliLogs: !prod,
+  build: {
+  },
+  resolve: {
+    alias: {
+      counter: path.join(__dirname, 'src/components/counter'),
+      '@': path.join(__dirname, 'src')
+    },
+    aliasFields: ['wepy', 'weapp'],
+    modules: ['node_modules']
+  },
+  compilers: {
+    less: {
+      compress: true,
+      plugins: [new LessPluginAutoPrefix({browsers: ['Android >= 2.3', 'Chrome > 20', 'iOS >= 6']})]
+    },
+    // sass: {
+    //   outputStyle: 'compressed'
+    // },
+    babel: {
+      sourceMap: true,
+      presets: [
+        'env'
+      ],
+      plugins: [
+        'transform-class-properties',
+        'transform-decorators-legacy',
+        'transform-object-rest-spread',
+        'transform-export-extensions',
+        'syntax-export-extensions'
+      ]
+    }
+  },
+  plugins: {
+  },
+  appConfig: {
+    noPromiseAPI: ['createSelectorQuery']
+  }
+}
+
+if (prod) {
+  // 压缩sass
+  // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
+
+  // 压缩js
+  module.exports.plugins = {
+    uglifyjs: {
+      filter: /\.js$/,
+      config: {
+      }
+    },
+    imagemin: {
+      filter: /\.(jpg|png|jpeg)$/,
+      config: {
+        jpg: {
+          quality: 80
+        },
+        png: {
+          quality: 80
+        }
+      }
+    }
+  }
+}
